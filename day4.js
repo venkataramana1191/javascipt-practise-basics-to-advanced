5e1e9a2f5fc6000b66fa6b9e91a5ecb42f011e22
venkataramana1191 <venkataramanab16@gmail.com>
2020-09-10 22:10:03 +0530

    Js exercise and task for Day4 completed

--- day4.js
+++ day4.js
@@ -18,9 +18,98 @@ function doSomething(){
    
     change.src=newUrl;
   }
-  function getName(){
-    const name=document.getElementById('gname');
-    const nameinput=document.getElementsByClassName('inputClass');
-    console.log(name);
-    console.log(nameinput[0].value);
+function changeImage1(){
+    const change=document.getElementById('image');
+    const newUrl="https://pbs.twimg.com/profile_images/1250978089997393922/AfVVOKcB.jpg";
+   
+    change.src=newUrl;
   }
+  function defaultImage(){
+    const change=document.getElementById('image');
+    const newUrl="https://3.bp.blogspot.com/-RgRq4imEpOI/WacIKXOJP4I/AAAAAAAABBw/DX3evXQtQTUAvZluPC2UEEHE0jq6VeWMwCEwYBhgL/s1600/IMG_20170829_211128.jpg";
+   
+    change.src=newUrl;
+  }
+
+  function getName(){
+    let nameinput=document.getElementsByClassName('input');
+    // console.log(nameinput[0].value)
+    nameinput[1].value=nameinput[0].value;
+    // console.log(nameinput[1].value)
+  }
+  // name (string), age (number), country (string), hobbies array (string [ ] )
+  persons=[
+    {
+      name: "Venkat",
+      age:22,
+      country:"India",
+      hobbies: [
+        "Playing chess", "doing web projects" ,"playing Hocky"
+      ]
+    },
+    {
+      name: "Neha",
+      age:27,
+      country:"America",
+      hobbies: [
+        "Tech Talks", "watching movies" ,"playing Video games","Kindle reading"
+      ]
+    },
+    {
+      name: "Srinu",
+      age:26,
+      country:"Chicago",
+      hobbies: [
+        "watching movies" ,"Reading Books","Cricket"
+      ]
+    },
+    {
+      name: "Anand",
+      age:26,
+      country:"England",
+      hobbies: [
+        "Listening to music","playing Video games","cards"
+      ]
+    },
+    {
+      name: "Prasad",
+      age:19,
+      country:"Gana",
+      hobbies: [
+        "Eating variety food","playing Video games","Cricket"
+      ]
+    },
+    {
+      name: "Varaprasad",
+      age:49,
+      country:"India",
+      hobbies: [
+        "Bible Reading","Preaching","free advices"
+      ]
+    },
+    {
+      name: "Sujatha",
+      age:45,
+      country:"India",
+      hobbies: [
+        "Bible Reading","listening to music","Help"
+      ]
+    }
+  ]
+   persons.forEach(element => {
+     console.log(`Name:${element.name} ,Age:${element.age}, Country:${element.country},\n Hobbies:${element.hobbies}`)
+   });
+   console.log("Age<30 persons data\n" )
+   function ageLessthan30(persons){
+    persons.forEach(element => {
+      if(element.age<30){
+       console.log(`Name:${element.name} ,Age:${element.age}, Country:${element.country},\n Hobbies:${element.hobbies}`)}
+       });
+   }
+   function countryIndia(persons){
+    persons.forEach(element => {
+      if(element.country=="India"){
+       console.log(`Name:${element.name} ,Age:${element.age}, Country:${element.country},\n Hobbies:${element.hobbies}`)}
+       });
+   }
+   
